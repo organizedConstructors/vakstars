90da93aff0e6d8ed6b8af71a4c7cdb9f0045172a
Zoltán Adamek <zadamek@newpush.com>
2013-05-07 00:26:25 +0200

    New date handling; query for date ranges, etc.
    Also modifies script for new deployment config.

--- points-to-web.sh
+++ points-to-web.sh
@@ -1,6 +1,6 @@
 # This is a sample for moving data out from the database with VAKstars
 # to a sample Jekyll installation.
 
-./vakstars.py dump-log-table > ~/Weboldalak/vakstars/_includes/csillagnaplo-sorok.md
-./vakstars.py dump-points-table > ~/Weboldalak/vakstars/_includes/allas-sorok.md
+./vakstars.py dump-log-table > ../VAKstars\ oldal/vakstars/_includes/csillagnaplo-sorok.md
+./vakstars.py dump-points-table > ../VAKstars\ oldal/vakstars/_includes/allas-sorok.md
 
--- vakstars.py
+++ vakstars.py
@@ -3,11 +3,7 @@
 
 import sys
 import sqlite3
-# from datetime import date
-
-# TODO: normális dátum kezelés
-# TODO: paraméterek ellenőrzése
-# TODO: batch kezelés
+import re
 
 db = None
 c = None
@@ -16,6 +12,7 @@ plus_receiver = 10
 minus_sender = -1
 minus_receiver = -2
 
+
 def prepare_db():
     """Prepares the database: connect, prepare cursor"""
     global db, c
@@ -23,9 +20,19 @@ def prepare_db():
     db.row_factory = sqlite3.Row
     c = db.cursor()
 
+
 def leave_db():
     """Ends database connection"""
     c.close()
+
+
+def natural_date_to_sql_datestring(natural_date):
+    matches = re.match(r"([0-9]*)[^0-9]*([0-9]*)[^0-9]*([0-9]*)[^0-9]*", natural_date)
+    datestring = "{year}-{month}-{day}".format(year=matches.group(1),
+                                               month=matches.group(2),
+                                               day=matches.group(3))
+    return datestring
+
 
 def insert_profile(name, startdate):
     """Inserts a new profile."""
@@ -38,8 +45,9 @@ def insert_profile(name, startdate):
         INSERT INTO profiles(name, startdate)
         VALUES (:name, :startdate)
         """,
-        {'name': name, 'startdate': startdate})
+              {'name': name, 'startdate': startdate})
     db.commit()
+
 
 def delete_votes_by_profile_id(id):
     """Deletes all votes (given and received) for a profile id."""
@@ -50,6 +58,7 @@ def delete_votes_by_profile_id(id):
         """, {'id': id})
     db.commit()
 
+
 def delete_profile(id):
     """Deletes a profile by id."""
     global c, db
@@ -59,6 +68,7 @@ def delete_profile(id):
         """, {'id': id})
     db.commit()
     delete_votes_by_profile_id(id)
+
 
 def profile_id_by_name(name):
     """Returns profile id by name."""
@@ -78,6 +88,7 @@ def profile_id_by_name(name):
             """, {'name': name})
         return c.fetchone()['id']
 
+
 def profile_name_by_id(id):
     """Returns name by id."""
     global c
@@ -87,7 +98,8 @@ def profile_name_by_id(id):
             """, {'id': id})
     return c.fetchone()['name']
 
-def vote(sender, receivers, date, reason, type = "1"):
+
+def vote(sender, receivers, date, reason, type="1"):
     """File(s) vote(s).
 
     If the receivers is iterable, everyone in the list will receive a vote.
@@ -100,20 +112,30 @@ def vote(sender, receivers, date, reason, type = "1"):
             INSERT INTO votes(sender, receiver, date, reason, type)
             VALUES (:sender, :receiver, :date, :reason, :type)
             """,
-            {'sender': sender, 'receiver': receivers,
-             'date': date, 'reason': reason, 'type': type})
+                  {'sender': sender, 'receiver': receivers,
+                   'date': date, 'reason': reason, 'type': type})
         db.commit()
 
-def get_vote_log():
+def get_vote_log(start_date, end_date):
     """Creates vote log dict from db."""
     global c, db
 
-    votelog = []
+    vote_log = []
     points = {}
-    c.execute("""
-        SELECT type, sender, receiver, date, reason
-        FROM votes
-        """)
+
+    if start_date is None and end_date is None:
+        c.execute("""
+            SELECT type, sender, receiver, date, reason
+            FROM votes
+            """)
+    else:
+        c.execute("""
+            SELECT type, sender, receiver, date, reason
+            FROM votes
+            WHERE date(date) >= date(:start_date) and
+                date(date) <= date(:end_date)
+            """,
+                  {'start_date': start_date, 'end_date': end_date})
     for row in c:
         if row["sender"] not in points:
             points[row["sender"]] = 0
@@ -143,8 +165,9 @@ def get_vote_log():
             'receiver_points_before': receiver_original_points,
             'receiver_points_after': points[row["receiver"]],
         }
-        votelog.append(vote)
-    return {'vote_log': votelog, 'points': points}
+        vote_log.append(vote)
+    return {'vote_log': vote_log, 'points': points}
+
 
 html_escape_table = {
     "&": "&amp;",
@@ -152,11 +175,13 @@ html_escape_table = {
     "'": "&apos;",
     ">": "&gt;",
     "<": "&lt;",
-    }
+}
+
 
 def html_escape(text):
     """Produce entities within text."""
-    return "".join(html_escape_table.get(c,c) for c in text)
+    return "".join(html_escape_table.get(c, c) for c in text)
+
 
 def vote_log_to_log_table_html(vote_log):
     """Returns the log table in HTML."""
@@ -165,30 +190,31 @@ def vote_log_to_log_table_html(vote_log):
             typesign = "+"
         else:
             typesign = "-"
-        print('<tr><td>{date}</td>'\
-              '<td>{type}</td>'\
-              '<td>{sender} '\
-              '({sender_points_before} &rarr; {sender_points_after})</td>'\
-              '<td>{receiver} '\
-              '({receiver_points_before} &rarr; {receiver_points_after})</td>'\
+        print('<tr><td>{date}</td>' \
+              '<td>{type}</td>' \
+              '<td>{sender} ' \
+              '({sender_points_before} &rarr; {sender_points_after})</td>' \
+              '<td>{receiver} ' \
+              '({receiver_points_before} &rarr; {receiver_points_after})</td>' \
               '<td>{reason}</td></tr>'.format(
-            date = row["date"].encode('utf-8'),
-            type = typesign,
-            sender = profile_name_by_id(row["sender"]).encode('utf-8'),
-            sender_points_before = row["sender_points_before"],
-            sender_points_after = row["sender_points_after"],
-            receiver = profile_name_by_id(row["receiver"]).encode('utf-8'),
-            receiver_points_before = row["receiver_points_before"],
-            receiver_points_after = row["receiver_points_after"],
-            reason = html_escape(row['reason'].encode('utf-8'))
+            date=row["date"].encode('utf-8'),
+            type=typesign,
+            sender=profile_name_by_id(row["sender"]).encode('utf-8'),
+            sender_points_before=row["sender_points_before"],
+            sender_points_after=row["sender_points_after"],
+            receiver=profile_name_by_id(row["receiver"]).encode('utf-8'),
+            receiver_points_before=row["receiver_points_before"],
+            receiver_points_after=row["receiver_points_after"],
+            reason=html_escape(row['reason'].encode('utf-8'))
         ))
+
 
 def vote_log_to_points_table_html(vote_log):
     """Returns the sorted points table in HTML."""
     sorted_votelog = sorted(
         vote_log['points'].items(),
-        key = lambda x: x[1],
-        reverse = True
+        key=lambda x: x[1],
+        reverse=True
     )
     position = 0
     previous = None
@@ -196,14 +222,15 @@ def vote_log_to_points_table_html(vote_log):
         if position == 0 or (previous != None and previous != profile[1]):
             position += 1
         print("{list_position}. {name} - {points}".format(
-            list_position = position,
-            name = profile_name_by_id(profile[0]).encode('utf-8'),
-            points = profile[1]
+            list_position=position,
+            name=profile_name_by_id(profile[0]).encode('utf-8'),
+            points=profile[1]
         ))
         previous = profile[1]
 
 def import_tsv(filename):
     import csv
+
     with open(filename, 'rb') as tsvfile:
         tsvreader = csv.reader(tsvfile, delimiter='\t')
         tsvreader.next()
@@ -228,7 +255,7 @@ def import_tsv(filename):
                 sender = profile_id_by_name(row[0].decode('utf-8'))
                 receiver = profile_id_by_name(row[1].decode('utf-8'))
                 reason = row[2].decode('utf-8')
-                date = row[3].decode('utf-8')
+                date = natural_date_to_sql_datestring(row[3].decode('utf-8'))
                 vote(sender, receiver, date, reason)
                 votes += 1
             print("Sikeresen felvittem {number_of_votes} szavazatot.".format(number_of_votes=votes))
@@ -242,10 +269,17 @@ def help():
     vakstars.py tsv-import <.tsv fájl>
     vakstars.py dump-log-table
     vakstars.py dump-points-table
+    vakstars.py dump-daterange-stat <kezdő dátum> <befejező dátum>
         """)
 
+
 def select_operation(operation):
-    operations = ['dump-log-table', 'dump-points-table', 'register', 'vote', 'tsv-import']
+    operations = ['dump-log-table',
+                  'dump-points-table',
+                  'dump-daterange-stat',
+                  'register',
+                  'vote',
+                  'tsv-import']
     if operation not in operations:
         raise Exception('Ilyen műveletünk nincs is!')
     else:
@@ -255,6 +289,11 @@ def select_operation(operation):
 
         if operation == "dump-points-table":
             vote_log_to_points_table_html(get_vote_log())
+
+        if operation == "dump-daterange-stat":
+            start_date = natural_date_to_sql_datestring(sys.argv[2].decode('utf-8'))
+            end_date = natural_date_to_sql_datestring(sys.argv[3].decode('utf-8'))
+            vote_log_to_points_table_html(get_vote_log(start_date, end_date))
 
         if operation == "register":
             name = sys.argv[2].decode('utf-8')
@@ -292,14 +331,14 @@ def select_operation(operation):
                 elif found_at == starting_parameter_index:
                     raise Exception('Szintaxishiba: nincsen fogadó fél a listában!')
                 else:
-                    receivers = sys.argv[starting_parameter_index : found_at]
+                    receivers = sys.argv[starting_parameter_index: found_at]
                     receivers = [profile_id_by_name(s.decode('utf-8')) for s in receivers]
                     argument_continue = found_at + 1
 
             date = sys.argv[argument_continue].decode('utf-8')
             reason = sys.argv[argument_continue + 1].decode('utf-8')
 
-            vote(sender, receivers, date, reason,type)
+            vote(sender, receivers, date, reason, type)
 
         leave_db()
 
